06281120c245ea05d5e0629c11b7e1555d9bda44
trieadriana <162008149+trieadriana@users.noreply.github.com>
2024-03-03 15:01:03 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./signUp.css">
+    <title>Bellamadu</title>
+    <style>
+        .form{
+            width: 300px;
+            height: 290px;
+        }
+    </style>
+</head>
+<body>
+    <div class="form">
+        <div class="title">
+            <p>Login Admin</p>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="Username" placeholder="Name">
+            <input type="text" name="Password" placeholder="Password">
+            <input type="submit" value="Login" name="Login_button">
+        </form>
+    </div>
+</body>
+</html>
---
+++ signUp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./signUp.css">
+    <title>Bellamadu</title>
+</head>
+<body>
+    <div class="form">
+        <div class="title">
+            <p>Sign Up Admin</p>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="Username" placeholder="Name">
+            <input type="text" name="Password" placeholder="Password">
+            <input type="text" name="ConfiPassword" placeholder="Confirm Password">
+            <input type="submit" value="SignUp" name="SignUp_button">
+        </form>
+    </div>
+</body>
+</html>
